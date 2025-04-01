215ebd173c12c107c0cdfb9db258f6ff6d48d269
demouser2002 <demo2002email@gmail.com>
2025-04-02 00:09:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,9 +15,9 @@ since="2025-01-01T01:01:00Z"
 #since=$(date +"%Y-%m-%dT%H:%M:%SZ")
 after=""
 
-hasNextPage=true
-while [ $hasNextPage ]
-do
+#hasNextPage=true
+#while [ $hasNextPage ]
+#do
 gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F after=$after -f query='
 query issues ($owner: String!, $name: String!, $since: DateTime!, $after: String) {
 		repository(owner:$owner, name: $name) {
@@ -79,8 +79,10 @@ query issues ($owner: String!, $name: String!, $since: DateTime!, $after: String
  echo $responseData
 
  hasNextPage=responseData.data.repository.issues.pageInfo.hasNextPage
+ echo $hasNextPage
  after=responseData.data.repository.issues.pageInfo.endCursor
-done
+ echo $after
+#done
 
 gh api graphql -F owner='{owner}' -F name='{repo}' -F start_date=$start_date -f query='
   query($name: String!, $owner: String!,$start_date: GitTimestamp!){
