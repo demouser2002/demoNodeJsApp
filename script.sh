ce49274923705ae7dab6b9fe762f5c8808d90da7
demouser2002 <demo2002email@gmail.com>
2025-04-01 22:07:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,8 +12,8 @@ gh issue list
 start_date="2025-01-01T01:01:00"
 end_date="2025-03-01T01:01:00"
 
-gh api graphql -F owner='{owner}' -F name='{repo}' -f query='
-  query($name: String!, $owner: String!){
+gh api graphql -F owner='{owner}' -F name='{repo}' -F start_date='{start_date}' -f query='
+  query($name: String!, $owner: String!,$start_date: DateTime!){
     repository(owner:$owner, name:$name) {
             defaultBranchRef {
                 target {
