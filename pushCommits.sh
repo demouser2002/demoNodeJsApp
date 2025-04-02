61fd4cbe58ba6bfc79c12ab6de6e684809f6de6d
demouser2002 <demo2002email@gmail.com>
2025-04-02 13:15:15 +0530

    Update pushCommits.sh

--- pushCommits.sh
+++ pushCommits.sh
@@ -10,7 +10,7 @@ since=$(date --date="$days days ago" +"%Y-%m-%dT%H:%M:%S")
 after=null
 
 hasNextPage=true
-while [ $hasNextPage == true ]
+while [ $hasNextPage ]
 do
 gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F after=$after -f query='
   query($name: String!, $owner: String!,$since: GitTimestamp!, $after: String){
