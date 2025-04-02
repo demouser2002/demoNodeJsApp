da5ecde1ab83cfe825491fdb4eebe06645a1702d
demouser2002 <demo2002email@gmail.com>
2025-04-02 18:26:41 +0530

    Update pushCommits.sh

--- pushCommits.sh
+++ pushCommits.sh
@@ -12,13 +12,13 @@ after=null
 hasNextPage=true
 while [ $hasNextPage ]
 do
-gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F after=$after -f query='
+gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F after="$after" -f query='
   query($name: String!, $owner: String!,$since: GitTimestamp!, $after: String){
     repository(owner:$owner, name:$name) {
             defaultBranchRef {
                 target {
                     ... on Commit {
-                        history(since: $since, first: 2, after: "$after") {
+                        history(since: $since, first: 2, after: $after) {
                             totalCount
                             nodes {
                                 additions
