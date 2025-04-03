e69b9de8976bdf6a8cbb68f6c3cd50930d387661
demouser2002 <demo2002email@gmail.com>
2025-04-03 15:03:04 +0530

    Update pushToDI.sh

--- pushToDI.sh
+++ pushToDI.sh
@@ -1,11 +1,11 @@
 
 #!/usr/bash
 
-if [[ $# = 0 ]]; then
+if [ $# = 0 ]; then
     ISSUES="true"
     COMMITS="true"
     PULLREQUESTS="true"
-elif [[ $# > 0 ]]; then
+elif [ $# > 0 ]; then
     for type in "$@"
     do
         case "$type" in
@@ -16,17 +16,17 @@ elif [[ $# > 0 ]]; then
     done
 fi
 
-if [[ "$ISSUES" = "true" ]]; then
+if [ "$ISSUES" = "true" ]; then
     sh pushIssues.sh
      echo "Issues pushed to DevOps Intelligence"
 fi
 
-if [[ "$COMMITS" = "true" ]]; then
+if [ "$COMMITS" = "true" ]; then
     sh pushCommits.sh
      echo "Commits pushed to DevOps Intelligence"
 fi
 
-if [[ "$PULLREQUESTS" = "true" ]]; then
+if [ "$PULLREQUESTS" = "true" ]; then
     sh pushPRDetails.sh
      echo "Issues pushed to DevOps Intelligence"
 fi
