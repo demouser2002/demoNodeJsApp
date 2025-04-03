ac13b595ecdb58dea64e62f9e0c32f6e116980e4
demouser2002 <demo2002email@gmail.com>
2025-04-03 20:22:26 +0530

    Update pushToDI.sh

--- pushToDI.sh
+++ pushToDI.sh
@@ -2,7 +2,7 @@
 #!/usr/bash
 
 DAYS=${DAYS:-30}
-echo $DAYS
+echo "********Pushing data to DevOps Intelligence for the last"$DAYS" days************"
 
 if [ $# = 0 ]; then
     ISSUES="true"
@@ -21,27 +21,27 @@ fi
 
 if [ "$ISSUES" = "true" ]; then
      if sh pushIssues.sh $DAYS; then
-        echo "Issues pushed to DevOps Intelligence"
+        echo "*************Issues successfully pushed to DevOps Intelligence**************"
     else
-        echo "Error: Failed to push issues to DevOps Intelligence"
+        echo "*********Error: Failed to push issues to DevOps Intelligence****************"
         exit 1
     fi    
 fi
 
 if [ "$COMMITS" = "true" ]; then
      if sh pushCommits.sh $DAYS; then
-        echo "Commits pushed to DevOps Intelligence"
+        echo "*************Commits successfully pushed to DevOps Intelligence*************"
     else
-        echo "Error: Failed to push commits to DevOps Intelligence"
+        echo "*********Error: Failed to push commits to DevOps Intelligence***************"
         exit 1
     fi    
 fi
 
 if [ "$PULLREQUESTS" = "true" ]; then
      if sh pushPRDetails.sh $DAYS; then
-        echo "Pull Requests pushed to DevOps Intelligence"
+        echo "********Pull Requests Details successfully pushed to DevOps Intelligence*****"
     else
-        echo "Error: Failed to push pull requests to DevOps Intelligence"
+        echo "*****Error: Failed to push pull requests details to DevOps Intelligence******"
         exit 1
     fi    
 fi
