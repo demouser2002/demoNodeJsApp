bcfd7ede6652fdd97cd55d0669be0eb6c19ecfaa
demouser2002 <demo2002email@gmail.com>
2025-04-03 14:55:00 +0530

    Update and rename script.sh to pushToDI.sh

---
+++ pushToDI.sh
@@ -0,0 +1,39 @@
+
+#!/usr/bash
+
+set -e
+
+if [[ "${PUSH_DEBUG:-false}" == "true" ]]; then
+    set -x
+fi
+
+if [[ $# == 0 ]]; then
+    ISSUES="true"
+    COMMITS="true"
+    PULLREQUESTS="true"
+elif [[ $# > 0 ]]; then
+    for type in "$@"
+    do
+        case "$type" in
+            issues) ISSUES="true";;
+            commits) COMMITS="true";;
+            pullrequests) PULLREQUESTS="true";;
+        esac
+    done
+fi
+
+if [[ "$ISSUES" == "true" ]]; then
+    sh pushIssues.sh
+     echo "Issues pushed to DevOps Intelligence"
+fi
+
+if [[ "$COMMITS" == "true" ]]; then
+    sh pushCommits.sh
+     echo "Commits pushed to DevOps Intelligence"
+fi
+
+if [[ "$PULLREQUESTS" == "true" ]]; then
+    sh pushPRDetails.sh
+     echo "Issues pushed to DevOps Intelligence"
+fi
+
