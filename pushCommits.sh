416931485e30927db581144549c143eee48b1674
demouser2002 <demo2002email@gmail.com>
2025-04-03 21:21:23 +0530

    Update pushCommits.sh

--- pushCommits.sh
+++ pushCommits.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <number_of_days>"
-    exit 1
-fi
+since=$1
+until=$2
 
-days=$1
-since=$(date --date="$days days ago" +"%Y-%m-%dT%H:%M:%S")
 after=null
 commitsPushed=0
 commitBatchSize=100
@@ -15,13 +11,13 @@ hasNextPage="true"
 echo '************* Pushing Commits ***************************************'
 while [ $hasNextPage = "true" ]
 do
-gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F  after="$after" -f query='
-  query($name: String!, $owner: String!,$since: GitTimestamp!, $after: String){
+gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since  -F until=$until -F  after="$after" -f query='
+  query($name: String!, $owner: String!,$since: GitTimestamp!, $until: GitTimestamp!, $after: String){
     repository(owner:$owner, name:$name) {
             defaultBranchRef {
                 target {
                     ... on Commit {
-                        history(since: $since, first: 100, after: $after) {
+                        history(since: $since, until: $until, first: 100, after: $after) {
                             totalCount
                             nodes {
                                 additions
