110fc3ecb7cea9180bfd6763b7dc73375467ad7e
demouser2002 <demo2002email@gmail.com>
2025-04-03 18:34:58 +0530

    Update pushCommits.sh

--- pushCommits.sh
+++ pushCommits.sh
@@ -58,4 +58,9 @@ gh api graphql -F owner='{owner}' -F name='{repo}' -F since=$since -F  after="$a
  echo 'Has NextPage:' $hasNextPage
  after=`jq -r '.data.repository.defaultBranchRef.target.history.pageInfo.endCursor' commits.json`
  echo 'End Cursor:' $after
+
+ commitCount=`jq -r '.data.repository.defaultBranchRef.target.history.totalCount' commits.json`
+ if [ $after != null ]; then
+    echo 'Pushed ' $commitCount ' Commits to DevOps Intelligence'
+ fi
 done
